2b9b8977ad68c1cfc0fef2fa09ff724d7208209b
zhcosin <zhcosin@163.com>
2025-10-16 11:35:40 +0800

    启用 json layer

--- init.el
+++ init.el
@@ -34,6 +34,7 @@ This function should only modify configuration layer settings."
    dotspacemacs-configuration-layers
    '(yaml
      javascript
+     json
      sql
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
