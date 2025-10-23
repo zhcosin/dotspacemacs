736603ce7d8b132c7d07f3459b662f598330d2eb
zhcosin <zhcosin@163.com>
2025-10-23 16:19:58 +0800

    闆嗘垚 msys2 浣滀负 emacs 鐨?shell 浼氬奖鍝?latex 鐨勯瑙堝姛鑳斤紝鎵€浠ョ鐢?msys2 鐨勯泦鎴?

--- init.el
+++ init.el
@@ -702,14 +702,9 @@ before packages are loaded."
     (define-key evil-emacs-state-map (kbd "C-y") 'yank))
 
 
-  ;; 集成 msys2
-  (setq shell-file-name "D:/msys64/usr/bin/bash.exe")
-  (setq explicit-bash.exe-args '("--login" "-i"))  ;; 作为交互式登录shell启动
-  ;;(setenv "MSYSTEM" "MINGW64")
-  ;;(setenv "CHERE_INVOKING" "1")
-  ;;(setenv "MSYS2_PATH_TYPE" "inherit")
-  ;;(add-to-list 'exec-path "D:/msys64/usr/bin")
-  ;;(setenv "PATH" (concat "D:/msys64/mingw64/bin;" (getenv "PATH")))
+  ;; 集成 msys2，由于在 windows 上这个集成会改变 emacs 所使用的 shell，进而影响 latex 预览等功能，所以暂不启用
+  ;;(setq shell-file-name "D:/msys64/usr/bin/bash.exe")
+  ;;(setq explicit-bash.exe-args '("--login" "-i"))  ;; 作为交互式登录shell启动
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
